71d7e708004164b223d2071a6e1544f5b12ff858
Krish Munot <krishmunot@gmail.com>
2016-04-21 22:58:04 +0000

    function for generic messages

--- index.js
+++ index.js
@@ -63,6 +63,39 @@ function sendTextMessage(sender, text) {
 	})
 }
 
+function sendGenericMessage(sender) {
++	messageData = {
++		"attachment": {
++			"type": "template",
++			"payload": {
++				"template_type": "generic",
++				"elements": [{
++					"title": "First card",
++					"subtitle": "Element #1 of an hscroll",
++					"image_url": "http://messengerdemo.parseapp.com/img/rift.png",
++					"buttons": [{
++						"type": "web_url",
++						"url": "https://www.messenger.com",
++						"title": "web url"
++					}, {
++						"type": "postback",
++						"title": "Postback",
++						"payload": "Payload for first element in a generic bubble",
++					}],
++				}, {
++					"title": "Second card",
++					"subtitle": "Element #2 of an hscroll",
++					"image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
++					"buttons": [{
++						"type": "postback",
++						"title": "Postback",
++						"payload": "Payload for second element in a generic bubble",
++					}],
++				}]
++			}
++		}
++	}
+
 app.listen(app.get('port'), function() {
 	console.log('running on port', app.get('port'))
 })
